df216ef5a23dba22423648033faad74fe6cbdd5b
Matheus Amon <amonmatheus757@gmail.com>
2024-08-16 20:21:27 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar } from "./_components/ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
 
 const Home = () => {
     return (
@@ -28,6 +32,27 @@ const Home = () => {
                         className="rounded-xl object-cover"
                     />
                 </div>
+
+                <Card className="mt-6">
+                    <CardContent className="flex flex-row justify-between p-0">
+                        <div className="flex flex-col gap-2 py-5 pl-5">
+                            <Badge className="w-fit">Confirmado</Badge>
+                            <h3 className="font-semibold">Corte de Cabelo</h3>
+                            <div className="flex flex-row items-center gap-1">
+                                <Avatar className="h-6 w-6">
+                                    <AvatarImage src="https://utfs.io/f/7e309eaa-d722-465b-b8b6-76217404a3d3-16s.png" />
+                                </Avatar>
+                                <p className="text-sm">Barbearia FSW</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+                            <p className="text-sm">Agosto</p>
+                            <p className="text-xl">16</p>
+                            <p className="text-sm">20:00</p>
+                        </div>
+                    </CardContent>
+                </Card>
             </section>
         </>
     )
