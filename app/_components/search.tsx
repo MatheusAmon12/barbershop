aac44e076a8b0f92fbfd20e25503d32992dad196
Matheus Amon <amonmatheus757@gmail.com>
2024-08-27 18:52:22 -0300

    feat: ensure user can search for barbershops

---
+++ app/_components/search.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { SearchIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { useRouter } from "next/navigation"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+
+const formSchema = z.object({
+    search: z.string().trim().min(3, { message: "Digite algo para buscar!" }),
+})
+
+const Search = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            search: "",
+        },
+    })
+
+    const router = useRouter()
+
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        router.push(`/barbershops?search=${data.search}`)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} className="flex">
+                <FormField
+                    control={form.control}
+                    name="search"
+                    render={({ field }) => (
+                        <FormItem className="w-full">
+                            <FormControl>
+                                <Input
+                                    placeholder="Faça sua busca..."
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button variant="default" type="submit">
+                    <SearchIcon />
+                </Button>
+            </form>
+        </Form>
+    )
+}
+
+export default Search
